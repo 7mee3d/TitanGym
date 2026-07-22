76d27e48f0ce5c7d09c88b487b6f0cacb218a151
7mee3d <alanturing404@gmail.com>
2026-07-22 11:43:24 +0300

    Feat(Architecture): Implement 3-Tier architecture and integrate all layers

---
+++ TitanGym_DataAccessLayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TitanGym_DataAccessLayer
+{
+    public class Class1
+    {
+    }
+}
